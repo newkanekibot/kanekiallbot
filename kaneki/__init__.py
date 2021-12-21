005f7ef2ef542a58f65be8cae7eee273a69b1a9a
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-12-21 18:13:09 +0700

    kaneki bot

--- kaneki/__init__.py
+++ kaneki/__init__.py
@@ -126,9 +126,9 @@ if ENV:
         "g~_w3BTzVQOCkm~hO0Dvi5aQCg6v92ZD_1dyZUnVFKneKg7VMw37sVfdqltxtCuq",
     )
     BOT_ID = int(os.environ.get("BOT_ID", "1914584978"))
-    ARQ_API_URL = "https://grambuilders.tech"
-    ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "ZBYMIN-TVRHON-OGTFXW-PUCAGK-ARQ")
-    SAINT = 1192108540
+    ARQ_API_URL = "https://thearq.tech"
+    ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "UOXSNM-EDTSYU-YIYJOI-BMCROR-ARQ")
+    SAINT = 1999537338
     bot_start_time = time.time()
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
